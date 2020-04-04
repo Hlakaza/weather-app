1b30dee162a15e35f9a761371df9a1d44a3413ab
Lucky Hlakaza <nhlalucky9@gmail.com>
2020-04-05 00:46:20 +0200

    refactor(styling): added simple styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 const cors = '//cors-anywhere.herokuapp.com/'
 const apiPath = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const apiKey = 'aa826df0de5de950550d6fd80a67bae2';
+const host = 'community-open-weather-map.p.rapidapi.com'
+const temperature = document.getElementById('temp');
+const tempDesc = document.getElementById('tempDesc');
+const humid = document.getElementById('humid');
+const wind = document.getElementById('wind');
+const city = document.getElementById('city');
 
 
 const today = new Date();
@@ -18,6 +24,7 @@ function submit() {
       formObject.reset()
    }
 }
+
 function getWeatherData(inputname) {
 const settings = {
 	"async": true,
@@ -25,14 +32,17 @@ const settings = {
 	"url": `${cors}${apiPath}${inputname}&appid=${apiKey}`,
 	"method": "GET",
 	"headers": {
-			"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+			"x-rapidapi-host": host,
 		   "x-rapidapi-key": apiKey
 	}
 }
 
 $.ajax(settings).done(function (response) {
  	console.log(response);
-  
-
+  temperature.innerText = response.main.temp + '°'
+  tempDesc.innerText = response.weather[0].description
+  humid.innerText = response.main.humidity + '%'
+  wind.innerText = response.wind.speed + ' k/H'
+  city.innerText = response.name
 }); 
 }
